3b4c73bdb09e5b924f2e4ad8aff2d8e4ad39d238
Harshad0804 <harshadkarguwal@gmail.com>
2025-03-12 17:04:32 +0530

    assignment

---
+++ JAVASCRIPT/ex.js
@@ -0,0 +1,7 @@
+console.log("HELLO JAVASCRIPT")
+
+document.write("JS")
+
+document.getElementById("h1").innerHTML = "HELLO THIS IS JAVA";
+
+document.getElementById("h2").innerText = "HELLO THIS IS JAVA 3456";
