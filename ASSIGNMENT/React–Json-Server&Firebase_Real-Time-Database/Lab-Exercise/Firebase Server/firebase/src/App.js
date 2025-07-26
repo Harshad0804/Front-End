fecf3748a59e530af8b98095c9c338ee305e4a76
Harshad0804 <harshadkarguwal@gmail.com>
2025-07-27 00:21:17 +0530

    assignment

--- ASSIGNMENT/React–Json-Server&Firebase_Real-Time-Database/Lab-Exercise/Firebase Server/firebase/src/App.js
+++ ASSIGNMENT/React–Json-Server&Firebase_Real-Time-Database/Lab-Exercise/Firebase Server/firebase/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import AddProduct from './Pages/addProduct/AddProduct'
+import UpdateProduct from './Pages/updateproduct/UpdateProduct'
+import MyState from './context/data/MyState'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyState>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/addproduct' element={<AddProduct />} />
+          <Route path='/updateproduct' element={<UpdateProduct />} />
+        </Routes>
+      </BrowserRouter>
+    </MyState>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
